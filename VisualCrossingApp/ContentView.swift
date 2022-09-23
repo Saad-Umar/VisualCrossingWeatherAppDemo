3a642afa7bffcc4b3e7c45986a85a3cb284b93d1
Saad Umar <saad_umar93@yahoo.com>
2022-09-23 10:57:22 +0500

    Initial Commit

---
+++ VisualCrossingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisualCrossingApp
+//
+//  Created by Tixsee on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisualCrossingApp/VisualCrossingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualCrossingAppApp.swift
+//  VisualCrossingApp
+//
+//  Created by Tixsee on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct VisualCrossingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
